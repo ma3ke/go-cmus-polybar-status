38d33bbedf308aee0bc599ce8aff131a9c6150cf
Koen Westendorp <koensswestendorp+imacmerge@gmail.com>
2020-05-30 19:33:41 +0200

    Add parseDuration() function The parseDuration(int) -> (int, int) function takes an integer representing the duration in seconds, then returns a tuple of minutes, seconds. The tuple can then be used to create output formats like: *min*:*sec*.

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"os/exec"
 	"strings"
 	"log"
+	"math"
 )
 
 func main() {
 	status := getStatus()
 
 	fmt.Println(status[2])
+
+	fmt.Println(parseDuration(371))
 }
 
 func getStatus() []string {
@@ -26,4 +29,16 @@ func getStatus() []string {
 	output := strings.Split(string(status), "\n")
 
 	return output
+}
+
+func parseDuration(seconds int) (int, int){
+	if seconds < 0 {
+		return 0, 0
+	} else {
+		minutes := float64(seconds) / 60.0
+		min := math.Floor(minutes)
+		sec := math.Floor((minutes - min) * 60)
+
+		return int(min), int(sec)
+	}
 }
